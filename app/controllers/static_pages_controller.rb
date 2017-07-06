0e0b2eba3dc6ced1a8fe7cd5c5b1e69a8e397b9d
lnzd17 <lindsaykayedutton@gmail.com>
2017-07-06 14:27:47 -0400

    filtered whitespaces out of search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,14 +14,15 @@ class StaticPagesController < ApplicationController
   end
 
   def search
+    query = params[:query].gsub(/[[:space:]]/, '')
     begin
-      api_call = Gems.info(params[:query])
+      api_call = Gems.info(query)
     rescue JSON::ParserError
       api_call = { 'error_message' =>
                    "Oh no! Looks like that gem can't be found." }
     end
 
-    SearchResult.store_search_result(api_call, params[:query])
+    SearchResult.store_search_result(api_call, query)
 
     redirect_to root_path
   end
