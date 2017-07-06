b89ff00a48a02555f707a043bd1a617295fd77a0
lnzd17 <lindsaykayedutton@gmail.com>
2017-07-06 11:40:55 -0400

    added tests for custom class

--- spec/lib/search_result_spec.rb
+++ spec/lib/search_result_spec.rb
@@ -1,32 +1,59 @@
 require 'rails_helper'
 require 'search_result'
-require 'gem_result'
+require 'test_result'
+include TestData
 
 RSpec.describe 'SearchResult' do
+  before(:each) do
+    SearchResult.store_search_result(GEM_RESULT)
+  end
+
   describe 'return_result' do
     it 'returns stored a search_result' do
-      SearchResult.store_search_result(@gem_result)
-      expect(SearchResult.return_result).to eq(@gem_result);
+      expect(SearchResult.return_result).to eq(GEM_RESULT)
     end
   end
 
   describe 'result_created?' do
-    it 'confirms if a search_result has been stored'
+    it 'returns false if a search_result has not been stored' do
+      SearchResult.store_search_result(nil)
+      expect(SearchResult.result_created?).to eq(false)
+    end
+
+    it 'returns true if a search_result has been stored' do
+      expect(SearchResult.result_created?).to eq(true)
+    end
   end
 
   describe 'title' do
-    it 'returns the search_result title'
+    it 'returns the search_result title' do
+      expect(SearchResult.title).to eq(GEM_RESULT['name'])
+    end
   end
 
   describe 'info' do
-    it 'returns search_result info'
+    it 'returns search_result info' do
+      expect(SearchResult.info).to eq(GEM_RESULT['info'])
+    end
   end
 
   describe 'deps' do
-    it 'returns each search_result dependencies'
+    it 'returns each search_result dependencies' do
+      first_dep_name = GEM_RESULT['dependencies']['development'][0]['name']
+      expect(SearchResult.deps.first).to eq(first_dep_name)
+    end
   end
 
   describe 'result_error?' do
-    it 'confirms if error_message exists within search_result'
+    it 'returns true if error_message exists within search_result' do
+      ERROR_MESSAGE = { 'error_message' =>
+                        "Oh no! Looks like that gem can't be found." }
+      SearchResult.store_search_result(ERROR_MESSAGE)
+      expect(SearchResult.result_error?).to eq(true)
+    end
+
+    it 'returns false if error_message does not exist within search_result' do
+      expect(SearchResult.result_error?).to eq(false)
+    end
   end
 end
